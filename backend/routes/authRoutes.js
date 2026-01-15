5e36a62e2c7060bca257dde2ea649502b077d28f
snehabichkunde <bichkundesneha@gmail.com>
2026-01-15 15:11:12 +0530

    feat: add authentication middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 
 const { registerUser, loginUser } = require('../controller/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Register
 router.post('/register', registerUser);
 // Login
 router.post('/login', loginUser);
+
+// authentication
+router.get('/profile', authMiddleware, (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: req.user,
+  });
+});
+
+
 
 
 module.exports = router;
